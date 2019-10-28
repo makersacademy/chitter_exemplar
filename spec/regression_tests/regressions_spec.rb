e21a6ee4470e0749000e2e58c4443d10b730065d
David Mears <kieukmail@gmail.com>
2019-10-28 13:57:03 +0000

    Put test in appropriate file and directory

---
+++ spec/regression_tests/regressions_spec.rb
@@ -0,0 +1,12 @@
+feature "Regression tests:" do
+  scenario "clicking 'Sign Up' twice takes you to the correct url" do
+    visit "/"
+    click_link "Sign Up"
+    # Find something specific to the new page to confirm that it's finished loading
+    fill_in :email, with: "filler text"
+    click_link "Sign Up"
+
+    # Confirm that it doesn't go to a 404
+    expect(page).to have_content("Password Confirmation")
+  end
+end
