21147bfb5abe67a46268096ac8db42a198833f1f
David Mears <kieukmail@gmail.com>
2019-11-20 10:34:52 +0000

    Simplify test set-up

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -1,9 +1,6 @@
 feature "Navigation:" do
   scenario "clicking 'Sign Up' twice takes you to the correct url" do
-    visit "/"
-    click_link "Sign Up"
-    # Find something specific to the new page to confirm that it's finished loading
-    fill_in :email, with: "filler text"
+    visit "/users/new"
     click_link "Sign Up"
 
     # Confirm that it doesn't go to a 404
