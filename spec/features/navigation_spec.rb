d9ab7f1fb430c1be98769868adf4f32cb422f953
David Mears <kieukmail@gmail.com>
2019-11-20 10:39:02 +0000

    Add Sign In scenario test

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -6,4 +6,12 @@ feature "Navigation:" do
     # Confirm that it doesn't go to a 404
     expect(page).to have_content("Password Confirmation")
   end
+
+  scenario "clicking 'Sign In' twice takes you to the correct url" do
+    visit "/users/new"
+    click_link "Sign In"
+
+    # Confirm that it doesn't go to a 404
+    expect(page).to have_content("Password")
+  end
 end
